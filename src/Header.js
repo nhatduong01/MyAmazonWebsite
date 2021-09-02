b892e3891433da2e1fde6c499d451ed14c6d98e5
DuongNguyen <nhatduong30001@gmail.com>
2021-09-02 20:54:12 +0700

    Home Page

---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./header.css";
+import SearchIcon from "@material-ui/icons/Search";
+function Header() {
+  return (
+    <div className="header">
+      <img
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        className="header_logo"
+      />
+      <div className="header_search">
+        <input className="header_searchInput" type="text" />
+        <SearchIcon className="header_searchIcon" />
+      </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionLineOne">Hello Guests</span>
+          <span className="header_optionLineTwo">Sign in</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Returns</span>
+          <span className="header_optionLineTwo">& Orders</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Your</span>
+          <span className="header_optionLineTwo">Prime</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAHkNHvEgghKpDLETI2n-falQuLF4qoFME",
+    authDomain: "my-2dae3.firebaseapp.com",
+    projectId: "my-2dae3",
+    storageBucket: "my-2dae3.appspot.com",
+    messagingSenderId: "447304808219",
+    appId: "1:447304808219:web:b2c6e710f0c20a5ac845be",
+    measurementId: "G-DDCSK62EVG"
+  };
